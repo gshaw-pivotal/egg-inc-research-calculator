664f45ea2ef9bd9f966c2ed5001c23f6facf8112
gshaw-pivotal <gshaw+github@pivotal.io>
2018-07-16 23:27:03 -0400

    Calculator script now supports increasing research by multiple levels
    - Requires the user to ensure they have enter the total cost to upgrade
      for the number of levels specified (and not the cost for just one
      level of upgrade). Otherwise misleading outcomes will result.

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
-Egg Inc - Epic Research Calculator
+# Egg Inc - Epic Research Calculator
 
-# What is this?
+## What is this?
 
 It is a simple python script / app to determine which epic research upgrade you should take to get the best return for gold eggs spent.
 
@@ -8,7 +8,7 @@ Egg Inc has the 'Soul Food' epic research and the 'Prophecy Bonus' epic research
 
 This script helps to determine which upgrade path should be taken at a given point in time to maximise the bonus improvement obtained per gold egg spent.
 
-# How to use
+## How to use
 
 The script requires several inputs in order to make its determination.
 
@@ -19,14 +19,19 @@ currentNumberOfSE = 0 #Enter the current number of your soul eggs here
 currentNumberOfPE = 0 #Enter current number of your prophecy eggs here
 currentSoulEggResearchLevel = 0 #Enter the current level of your soul egg research here
 currentProphecyEggResearchLevel = 0 #Enter the current level of your prophecy egg research here
-costForNextSoulEggResearchLevel = 0 #The cost (in gold eggs) for the next level of soul egg (aka soul food) research here
-costForNextProphecyEggResearchLevel = 0 #The cost (in gold eggs) for the next level of prophecy egg research here
+
+numberOfLevelsToIncreaseSoulEggResearchBy = 1 #Enter the number of levels you wish to increase soul egg research by here
+numberOfLevelsToIncreaseProphecyEggResearchBy = 1 #Enter the number of levels you wish to increase prophecy egg research by here
+costForNextSoulEggResearchLevel = 0 #The total cost (in gold eggs) for the desired increase in levels of soul egg (aka soul food) research here
+costForNextProphecyEggResearchLevel = 0 #The total cost (in gold eggs) for the desired increase in levels of prophecy egg research here
 ```
 
 After entering all of the required values, run the script. It will perform a set of calculations, output results, with the final line indicating which upgrade should be taken next.
 
-# Limits
+## Limits
 
 1. The script relies on the inputs being correct and accurate in order to determine the best bonus increase per gold egg spent.
-1. It currently only makes the determination for the next level (calculates +1 to soul food and +1 to prophecy bonus).
-1. It does not yet take into account that increasing prophecy bonus generally cost significantly more per level than soul food and thus for the gold eggs spent increasing prophecy bonus by one, you could get several soul food levels.
+~~1. It currently only makes the determination for the next level (calculates +1 to soul food and +1 to prophecy bonus).~~
+~~1. It does not yet take into account that increasing prophecy bonus generally cost significantly more per level than soul food and thus for the gold eggs spent increasing prophecy bonus by one, you could get several soul food levels.~~
+
+The current version of this script supports calculating the improvement and cost for an increase of multiple research levels. This supports comparing for x number of gold eggs I can get A1 levels of one research versus A2 levels of another research. Overall, which would result in the best return for gold eggs spent?
--- ResearchCalculator.py
+++ ResearchCalculator.py
@@ -3,8 +3,11 @@ currentNumberOfSE = 0 #Enter the current number of your soul eggs here
 currentNumberOfPE = 0 #Enter current number of your prophecy eggs here
 currentSoulEggResearchLevel = 0 #Enter the current level of your soul egg research here
 currentProphecyEggResearchLevel = 0 #Enter the current level of your prophecy egg research here
-costForNextSoulEggResearchLevel = 0 #The cost (in gold eggs) for the next level of soul egg (aka soul food) research here
-costForNextProphecyEggResearchLevel = 0 #The cost (in gold eggs) for the next level of prophecy egg research here
+
+numberOfLevelsToIncreaseSoulEggResearchBy = 1 #Enter the number of levels you wish to increase soul egg research by here
+numberOfLevelsToIncreaseProphecyEggResearchBy = 1 #Enter the number of levels you wish to increase prophecy egg research by here
+costForNextSoulEggResearchLevel = 0 #The total cost (in gold eggs) for the desired increase in levels of soul egg (aka soul food) research here
+costForNextProphecyEggResearchLevel = 0 #The total cost (in gold eggs) for the desired increase in levels of prophecy egg research here
 message = "Upgrading "
 
 # Calculations for current bonus
@@ -13,13 +16,13 @@ currentBonusPerEgg = decimal.Decimal(1 + 0.05 + (0.01 * currentProphecyEggResear
 currentTotalBonus = currentBonusPerEgg * decimal.Decimal(currentNumberOfSE)
 
 # Calculations for raising soul egg (aka soul food) by one level
-bonusPerEggWithNextSoulEggResearchLevel = decimal.Decimal(1 + 0.05 + (0.01 * currentProphecyEggResearchLevel)) ** (decimal.Decimal(currentNumberOfPE)) * decimal.Decimal(10 + (currentSoulEggResearchLevel + 1))
+bonusPerEggWithNextSoulEggResearchLevel = decimal.Decimal(1 + 0.05 + (0.01 * currentProphecyEggResearchLevel)) ** (decimal.Decimal(currentNumberOfPE)) * decimal.Decimal(10 + (currentSoulEggResearchLevel + numberOfLevelsToIncreaseSoulEggResearchBy))
 totalBonusWithNextSoulEggResearchLevel = bonusPerEggWithNextSoulEggResearchLevel * decimal.Decimal(currentNumberOfSE)
 totalBonusImprovementWithNextSoulEggResearchLevel = decimal.Decimal(totalBonusWithNextSoulEggResearchLevel - currentTotalBonus)
 bonusGainedPerGoldEggSpentForNextSoulEggResearchLevel = decimal.Decimal(decimal.Decimal((totalBonusWithNextSoulEggResearchLevel - currentTotalBonus)) / decimal.Decimal(costForNextSoulEggResearchLevel))
 
 # Calculations for raising prophecy egg (aka prophecy bonus) by one level
-bonusPerEggWithNextProphecyEggResearchLevel = decimal.Decimal(1 + 0.05 + (0.01 * (currentProphecyEggResearchLevel + 1))) ** (decimal.Decimal(currentNumberOfPE)) * decimal.Decimal(10 + currentSoulEggResearchLevel)
+bonusPerEggWithNextProphecyEggResearchLevel = decimal.Decimal(1 + 0.05 + (0.01 * (currentProphecyEggResearchLevel + numberOfLevelsToIncreaseProphecyEggResearchBy))) ** (decimal.Decimal(currentNumberOfPE)) * decimal.Decimal(10 + currentSoulEggResearchLevel)
 totalBonusWithNextProphecyEggResearchLevel = bonusPerEggWithNextProphecyEggResearchLevel * decimal.Decimal(currentNumberOfSE)
 totalBonusImprovementWithNextProphecyEggResearchLevel = decimal.Decimal(totalBonusWithNextProphecyEggResearchLevel - currentTotalBonus)
 bonusGainedPerGoldEggSpentForNextProphecyEggResearchLevel = decimal.Decimal(decimal.Decimal((totalBonusWithNextProphecyEggResearchLevel - currentTotalBonus)) / decimal.Decimal(costForNextProphecyEggResearchLevel))
@@ -38,17 +41,17 @@ print("Current number of prophecy eggs: " + '{:,.0f}'.format(currentNumberOfPE))
 print("Current bonus per soul egg (%):  " + '{:,.0f}'.format(currentBonusPerEgg))
 print("Current Total bonus:             " + '{:,.0f}'.format(currentTotalBonus))
 print("----------------------------------------------------------------")
-print("If upgrade soul egg research by one level:")
-print("Bonus per soul egg (with + 1 SERL) (%):   " + '{:,.0f}'.format(bonusPerEggWithNextSoulEggResearchLevel))
-print("Total bonus (with + 1 SERL):              " + '{:,.0f}'.format(totalBonusWithNextSoulEggResearchLevel))
-print("Bonus improvement:                        " + '{:,.0f}'.format(totalBonusImprovementWithNextSoulEggResearchLevel))
-print("Bonus points per gold egg:                " + '{:,.0f}'.format(bonusGainedPerGoldEggSpentForNextSoulEggResearchLevel))
+print("If upgrade soul egg research by {} levels:".format(numberOfLevelsToIncreaseSoulEggResearchBy))
+print("Bonus per soul egg (with extra levels) (%):   " + '{:,.0f}'.format(bonusPerEggWithNextSoulEggResearchLevel))
+print("Total bonus (with extra levels):              " + '{:,.0f}'.format(totalBonusWithNextSoulEggResearchLevel))
+print("Bonus improvement:                            " + '{:,.0f}'.format(totalBonusImprovementWithNextSoulEggResearchLevel))
+print("Bonus points per gold egg:                    " + '{:,.0f}'.format(bonusGainedPerGoldEggSpentForNextSoulEggResearchLevel))
 print("----------------------------------------------------------------")
-print("If upgrade prophecy egg research by one level:")
-print("Bonus per soul egg (with + 1 PERL) (%):   " + '{:,.0f}'.format(bonusPerEggWithNextProphecyEggResearchLevel))
-print("Total bonus (with + 1 PERL):              " + '{:,.0f}'.format(totalBonusWithNextProphecyEggResearchLevel))
-print("Bonus improvement:                        " + '{:,.0f}'.format(totalBonusImprovementWithNextProphecyEggResearchLevel))
-print("Bonus points per gold egg:                " + '{:,.0f}'.format(bonusGainedPerGoldEggSpentForNextProphecyEggResearchLevel))
+print("If upgrade prophecy egg research by {} levels:".format(numberOfLevelsToIncreaseProphecyEggResearchBy))
+print("Bonus per soul egg (with extra levels) (%):   " + '{:,.0f}'.format(bonusPerEggWithNextProphecyEggResearchLevel))
+print("Total bonus (with extra levels):              " + '{:,.0f}'.format(totalBonusWithNextProphecyEggResearchLevel))
+print("Bonus improvement:                            " + '{:,.0f}'.format(totalBonusImprovementWithNextProphecyEggResearchLevel))
+print("Bonus points per gold egg:                    " + '{:,.0f}'.format(bonusGainedPerGoldEggSpentForNextProphecyEggResearchLevel))
 print("")
 print("================================================================")
 print(message)
